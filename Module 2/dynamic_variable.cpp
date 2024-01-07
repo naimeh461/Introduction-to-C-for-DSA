e05b7d63090fa0d60b409bdcbd52270061b99e1f
naimeh461 <naimeh461@gmail.com>
2024-01-08 00:14:47 +0600

    finished module 1

---
+++ Module 2/dynamic_variable.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // int x = 10;
+
+    int *a = new int;
+    *a = 10 ; //dereference
+    cout << *a <<endl;
+
+    float *f = new float;
+    *f = 1.5315;
+    cout<< *f <<endl;
+    return 0;
+}
